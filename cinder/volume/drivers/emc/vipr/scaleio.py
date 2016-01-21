41c476d61c49936493baa8769e75ed472a14c8ab
ravi edpuganti <ravi.edpuganti@emc.com>
2016-01-21 11:36:40 +0000

    2.4 SP1 northbound cinder driver

--- cinder/volume/drivers/emc/vipr/scaleio.py
+++ cinder/volume/drivers/emc/vipr/scaleio.py
@@ -102,7 +102,7 @@ class EMCViPRScaleIODriver(driver.VolumeDriver):
         """Driver entry point to get the export info for an existing volume."""
         pass
 
-    def create_export(self, context, volume):
+    def create_export(self, context, volume, connector=None):
         """Driver entry point to get the export info for a new volume."""
         pass
 
